9cdf72038d11a48fe103e9b786cdce19f904d298
Helia_Karisani <helia.karisani@gmail.com>
2025-06-09 12:34:43 -0400

    Initial commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,47 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    // creating states of the component
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+      // updating the states when a certain thing happens
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));  
+      },
+
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+
+      }
+    };
+
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+     <div className='content-rating'>
+        <p>  
+          Click on any rating button below and observe the number of clicks.
+        </p>
+        <div className='rating-buttons'>
+
+          <button className="like-button"  onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+          </button>
+          <button className="dislike-button"  onClick={this.state.handleDislike}>
+              Dislike ({this.state.dislikes})
+          </button>
+
+        </div>
+         <p>Total Ratings: {this.state.totalRatings}</p>
+     </div>
     );
   }
 }
